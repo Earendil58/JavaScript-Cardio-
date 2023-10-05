984b6d25e8e5eb10321432bbdcd57cd55ae07d08
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2023-10-05 11:01:37 -0300

    problem 2 start

---
+++ assets/js/code-training/problem_2.js
@@ -0,0 +1,6 @@
+// scheduleMeeting(..) should take a start time (in 24-hour format as a string "hh:mm") and 
+// a meeting duration (number of minutes). It should return true if the meeting falls entirely within the work day
+
+function scheduleMeeting(startTime) {
+
+}
