23b3a1ac2a33b01f19bc5578b2d5a73b2a9c7444
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2024-03-26 10:42:43 -0300

    function scope cardio

--- assets/js/functions/function.js
+++ assets/js/functions/function.js
@@ -3,3 +3,28 @@ const square = function (x) {
 };
 
 console.log(square(4));
+
+
+function upper(sentence) {
+    return sentence.toUpperCase();
+}
+
+console.log(upper(`I'm a sentence`))
+
+
+// FUNCTION SCOPE
+
+function something() {
+    const name = 'Carlos';
+    var city = 'Anillaco' // FUNCTION SCOPE // Not accessible from outside 
+    console.log(`My name is: ${name}`)
+
+
+    if (name === 'Carlos') {
+        let lastname = 'Menem' // BLOCK SCOPE
+        console.log(`The lastname is: ${lastname}`);
+    }
+}
+
+something();
+
