21ed7ef4e8a5e3e47dcb6aea338f72b45f876756
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2024-08-06 17:21:51 -0300

    mas ejercicios async

--- assets/js/async-code/async-await-4.js
+++ assets/js/async-code/async-await-4.js
@@ -1,151 +1,45 @@
-// function hacerPizza() {
+// function gitCall() {
 //     return new Promise((resolve, reject) => {
-//         const success = true;
-
-//         if (success) {
-//             resolve("It's working");
-//         }
-//         else {
-//             reject("Ops! Error detected");
-//         }
-//     })
-// }
-
-
-// const pizza1 = hacerPizza()
-
-// console.log(pizza1)
-
-// pizza1.then(resultado => {
-//     console.log(`Esto me devuelve el resultado de la promesa: ${resultado}`)
-// })
-
-
-// async function readPromise() {
-//     const promesa = await hacerPizza()
-//     console.log(`Esto es otra forma de esperar a una promesa: ${promesa}`);
-// }
-
-// readPromise();
-
-
-// function firstPromise() {
-//     const promesa = new Promise((resolve, reject) => {
 //         setInterval(() => {
-//             resolve('Corriendo proceso');
+//             const fetchData = fetch('https://api.github.com/users/Earendil58');
+//             resolve(fetchData)
 //         }, 2000)
-//     })
-
-//     return promesa
+//     });
 // }
 
-// const promesa1 = firstPromise()
-
-// promesa1.then(resultado => {
-//     console.log(resultado);
-// })
-
-// function numeroAleatorio() {
-//     return new Promise((resolve, reject) => {
-//         setInterval(() => {
-//             const numeroAleatorio = Math.floor(Math.random() * 100);
-//             resolve(numeroAleatorio)
-//         }, 1000)
-//     })
+// async function callParser() {
+//     const gitCallParser = await gitCall();
+//     const gitConverse = await gitCallParser.json();
+//     console.log(gitConverse);
 // }
 
-// const numeroRandom = numeroAleatorio();
+function apiCall() {
 
-// numeroRandom.then(resultado => {
-//     console.log(`Este es el primer resultado: ${resultado}`)
-//     return resultado * 2
-// })
-//     .then(resultado => {
-//         console.log(`Este es el segundo resultado: ${resultado}`);
-//         return resultado * 2
-//     })
-//     .then(resultado => {
-//         console.log(`Este es el segundo resultado: ${resultado}`);
-//         return resultado * 2
-//     })
+    const apiPromise = new Promise((resolve, reject) => {
+        const exito = Math.random() > 0.5;
+        const urlFetch = fetch('https://jsonplaceholder.typicode.com/todos/1');
 
-// function promesa3() {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             if (Math.random() < 0.5) {
-//                 resolve('La promesa se resolvió exitosamente');
-//             }
-//             else {
-//                 reject(new Error('La promesa fue rechazada'));
-//             }
-//         }, 1000)
-//     })
-// }
-
-
-// // const averiguarExito = promesa3()
-
-// // averiguarExito.then(resultado => {
-// //     console.log(resultado)
-// // })
-// //     .catch(err => { console.log(err) });
-
-// async function manejarPromesa3() {
-//     const leerPromesa3 = await promesa3();
-//     console.log(`Acá estoy leyendo la promesa 3: ${leerPromesa3}`);
-// }
-
-// manejarPromesa3();
-
-// function promesa4() {
-//     const promiseArray = new Promise((resolve, reject) => {
-//         const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-//         if (array) {
-//             resolve(array);
-//         }
-//         else {
-//             reject('No hay array');
-//         }
-//     })
-
-//     return promiseArray
-// }
-
-// const arrayPromesa = promesa4();
-
-// arrayPromesa.then(result => {
-//     const resultadoReduce = result.reduce((acc, num) => acc + num, 0);
-//     console.log(resultadoReduce);
-// });
-
-
-function promiseA() {
-    return new Promise((resolve, reject) => {
-        setInterval(() => {
-            resolve('Resuelta luego de 1 seg');
-        }, 1000)
-    });
-}
-
-function promiseB() {
-    const promiseB = new Promise(function (resolve) {
-        setInterval(() => {
-            resolve('Resuelta luego de 2 segs');
-        }, 2000)
+        if (exito) {
+            resolve(urlFetch);
+        }
+        else {
+            reject(new Error('Ups, no se pudo completar el llamado a la API'));
+        }
     })
 
-    return promiseB;
+    return apiPromise;
 }
 
-const promiseC = new Promise((resolve) => {
-    setInterval(() => {
-        resolve('Resuelta luego de 3 segs');
-    }, 3000);
-})
+async function apiCallParser() {
+    try {
+        const urlParser = await apiCall();
+        console.log(urlParser);
+        const urlJson = await urlParser.json();
+        console.log(urlJson);
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
 
-const promise1 = promiseA();
-const promise2 = promiseB();
-
-const promiseChain = Promise.all([promise1, promise2, promiseC]).then(resultados => {
-    console.log(resultados);
-})
+apiCallParser();
